19dfad51ed793a9950afa5c4cb6a41d800a67dfd
Ben Thatcher <149745752+btgs-0@users.noreply.github.com>
2026-02-16 05:22:13 +1030

    Update cdnewentry queries using prepared statements and remove useless FreeDB button. (#6)
    * Remove the FreeDB lookup and corresponding code (FreeDB is long defunct).
    
    * Use prepared statement in new CD entry form.
    
    * Fix form input so that new entry button actually works.
    
    * Format cdshow.php

--- cdnewentry.php
+++ cdnewentry.php
@@ -5,120 +5,110 @@
 ?>
 
 <HTML>
+
 <head>
-<TITLE>ThreeD - MusicDB New Entry</TITLE>
-<LINK REL="StyleSheet" HREF="style.css" TYPE="text/css">
+	<TITLE>ThreeD - MusicDB New Entry</TITLE>
+	<LINK REL="StyleSheet" HREF="style.css" TYPE="text/css">
 </head>
+
 <BODY onload="document.forms[0].xwords.focus()">
 
-<?php
-if (!$admin && $user['cdeditor'] != "t") {
-	echo "<p><font color=red><b>You do not have the necessary privileges to do that!</b></font><p>";
-	echo "</BODY></HTML>";
-	exit;
-}
-
-echo "<B>MUSIC CATALOGUE - NEW ENTRY</B>";
-
-echo "<p>You are about to add a new entry to the Catalogue.";
-echo "<br>Enter the Album name.";
-
-echo "<p><form action=cdnewentry.php method=post>";
-echo "<table border=0 cellspacing=0 cellpadding=4>";
-echo "<tr bgcolor=#CCCCCC>";
-echo "<td><b>Keywords</b></td>";
-$a = htmlentities(stripslashes($xwords));
-echo "<td><input type=text name=xwords value=\"$a\" size=50 maxlength=50></td>";
-echo "<td align=right>";
-echo "\n<input type=submit name=xsearch value=Search>\n";
-echo "</td>";
-echo "</tr>";
-echo "</table>";
-
-if (!$xwords || $xfreedbsearch) { echo "</form>"; }
-
-if ($xwords && !$xfreedbsearch) {
-	$query = "SELECT * FROM cd WHERE";
-	$words = preg_replace("/,/"," ",$xwords);
-	$words = preg_replace("/ +/"," ",$words);
-	$words = trim($words);
-	#echo "#$words#<p>";
-	$words = explode(" ", $words);
-	for ($i=0;$i<count($words);$i++) {
-		if ($i > 0) { $query .= " AND"; }
-		$query = $query . " (artist ~~* $q%$words[$i]%$q OR title ~~* $q%$words[$i]%$q OR genre ~~* $q%$words[$i]%$q OR company ~~* $q%$words[$i]%$q)";
+	<?php
+	if (!$admin && $user['cdeditor'] != "t") {
+		echo "<p><font color=red><b>You do not have the necessary privileges to do that!</b></font><p>";
+		echo "</BODY></HTML>";
+		exit;
 	}
-	$query = $query . " ORDER by UPPER(artist), UPPER(title);";
-	$result = pg_query($db, $query);
-	$num = pg_num_rows($result);
-	echo "<p><hr><p><b>$num match";
-	if ($num != 1) { echo "es"; }
-	
-	echo " found in the Catalogue</b>";
-	echo "<p>If it is not here try a ";
-	echo "<input type=submit name=xfreedbsearch value=\"FreeDB Lookup\"> or";
-	echo "</form>";
-	echo "<p><form action=cdedit.php method=post>";
-	echo "<input type=submit name=xnewentry value=\"Create a New Entry\">";
-	echo " with ";
-	echo "<input type=text name=xtr value=10 size=4 maxlength=10></td>";
-	echo " Tracks.";
-	echo "</form>";
-	
-	echo "<p><HR>";
-	if ($num) {
-		for ($i=0;$i<$num;$i++) {
-			$r = pg_Fetch_array($result, $i, PGSQL_ASSOC);
-			
-			$a = htmlentities($r['artist']);
-			$b = htmlentities($r['title']);
-			
-			if ($i) { echo "<br>"; } else { echo "<p>"; }
-			echo "<a HREF=cdshow.php?xref=" . $r['id'];
-			echo ">$a / $b<a>\n";
+
+	echo "<B>MUSIC CATALOGUE - NEW ENTRY</B>";
+
+	echo "<p>You are about to add a new entry to the Catalogue.";
+	echo "<br>Enter the Album name.";
+
+	echo "<p><form action=cdnewentry.php method=post>";
+	echo "<table border=0 cellspacing=0 cellpadding=4>";
+	echo "<tr bgcolor=#CCCCCC>";
+	echo "<td><b>Keywords</b></td>";
+	$a = htmlentities(stripslashes($xwords));
+	echo "<td><input type=text name=xwords value=\"$a\" size=50 maxlength=50></td>";
+	echo "<td align=right>";
+	echo "\n<input type=submit name=xsearch value=Search>\n";
+	echo "</td>";
+	echo "</tr>";
+	echo "</table>";
+
+	if (!$xwords) {
+		echo "</form>";
+	} else {
+		$words = preg_replace("/,/", " ", $xwords);
+		$words = preg_replace("/ +/", " ", $words);
+		$words = trim($words);
+		$words = explode(" ", $words);
+		$words_array = [];
+
+		$query = "
+			SELECT 
+				* 
+			FROM 
+				cd 
+			WHERE";
+
+		for ($i = 0; $i < count($words); $i++) {
+			// Push this word with wildcards into the array.
+			array_push($words_array, "%" . stripslashes($words[$i]) . "%");
+			// Define a corresponding parameter for the word just pushed onto the array.
+			$this_word_statement_identifier = "$" . ($i + 1);
+
+			if ($i > 0) {
+				$query .= " AND";
+			}
+
+			$query = $query . " (
+				artist ~~* $this_word_statement_identifier 
+				OR title ~~* $this_word_statement_identifier
+				OR genre ~~* $this_word_statement_identifier
+				OR company ~~* $this_word_statement_identifier
+			)";
+		}
+
+		$query = $query . " ORDER by UPPER(artist), UPPER(title);";
+		pg_prepare($db, "new_cd_exists_query", $query);
+
+		$result = pg_execute($db, "new_cd_exists_query", $words_array);
+		$num = pg_num_rows($result);
+		echo "<p><hr><p><b>$num match";
+		if ($num != 1) {
+			echo "es";
+		}
+
+		echo " found in the Catalogue</b>";
+		echo "</form>";
+		echo "<p><form action=cdedit.php method=post>";
+		echo "<input type=submit name=xnewentry value=\"Create a New Entry\">";
+		echo " with ";
+		echo "<input type=text name=xtr value=10 size=4 maxlength=10></td>";
+		echo " Tracks.";
+		echo "</form>";
+
+		echo "<p><HR>";
+		if ($num) {
+			for ($i = 0; $i < $num; $i++) {
+				$r = pg_Fetch_array($result, $i, PGSQL_ASSOC);
+
+				$a = htmlentities($r['artist']);
+				$b = htmlentities($r['title']);
+
+				if ($i) {
+					echo "<br>";
+				} else {
+					echo "<p>";
+				}
+				echo "<a HREF=cdshow.php?xref=" . $r['id'];
+				echo ">$a / $b<a>\n";
+			}
 		}
 	}
-}
-
-
-
-if ($xfreedbsearch && $xwords) {
-	echo "<form action=cdedit.php method=post>";
-	$stuff = stripslashes($xwords);
-	$stuff = urlencode ($stuff);
-	$ch = curl_init ("https://www.gracenote.com/search/?query=$stuff&search_type=album");
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER,1); 
-	curl_setopt ($ch, CURLOPT_HEADER, 0);
-	if ($webproxy) { curl_setopt ($ch, CURLOPT_PROXY, $webproxy); }
-	$aa = curl_exec ($ch);
-	curl_close ($ch);
-	$aa = preg_replace("/[\n\r]/"," ",$aa);
-	#echo $aa;
-	#preg_match_all ("|<div class=\"item\">.*?(<a href=.*?</a>).*?<div class=\"artist-name\"><strong>Artist\:</strong>(.*?)</div>|", $aa, $out);
-	#preg_match_all ("|<a href=\"/search/album_details.php\?tui_id=(.*?)&tui_tag=\">(.*?)</a>.*?<div class=\"artist-name\"><strong>Artist:</strong> (.*?)</div>|", $aa, $out);
-	preg_match_all ("|<div class=\"album-image\".*?<a href=\"/search/album_details.php\?tui_id=(.*?)&tui_tag.*?tui_tag=\">(.*?)</a>.*?<strong>Artist:</strong>.*?>(.*?)<|", $aa, $out);
-	
-	echo "<p><hr><p><b>".count($out[1])." match";
-	if (count($out[1]) != 1) { echo "es"; }
-	echo " found in the FreeDB database</b>";
-	echo "<p>If it is not here you will need to ";
-	echo "<input type=submit name=xnewentry value=\"Create a New Entry\">";
-	echo " with ";
-	echo "<input type=text name=xtr value=10 size=4 maxlength=10></td>";
-	echo " Tracks.";
-	echo "<p><HR>";
-	if (count($out[1])) {
-		for ($i=0;$i<count($out[1]);$i++) {
-			#echo "\n<tr><td>".$out[1][$i]."</td><td>".$out[2][$i]."</td><td>".$out[3][$i]."</td>";
-			if ($i) { echo "<br>"; } else { echo "<p>"; }
-			echo "<a href=cddblookup.php?id=".$out[1][$i].">".$out[3][$i]." / ".$out[2][$i]."<a>";
-			#echo htmlentities($out[1][$i] . "-" . $out[2][$i] . "-" . $out[3][$i]) . "<p><hr>";
-		}
-	}
-	echo "</form>";
-}
-
-
-?>
+	?>
 </BODY>
+
 </HTML>
